8815f4d6b02c38eca1cb698ed2fed822946729a0
gmanipon <gmanipon@jpl.nasa.gov>
2018-10-05 19:27:32 +0000

    run pylint

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "pytest", "scripttest", "mock", "flake8"
+        "pytest", "scripttest", "mock", "flake8", "pylint"
     ]
 )
